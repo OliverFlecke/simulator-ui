95094774395f7172d936892e23f0b27985675d26
Oliver Fleckenstein <oliverfl@live.dk>
2022-08-20 21:50:53 +0100

    feat: simple rendering of map

---
+++ src/api.ts
@@ -0,0 +1,28 @@
+const url = "http://localhost:8080";
+
+export function getEventSource(): EventSource {
+  return new EventSource(`${url}/stream`);
+}
+
+export async function getStaticMap(): Promise<World> {
+  return await fetch(`${url}/simulation/map`).then((x) => x.json());
+}
+
+// export interface Location {
+//   x: number;
+//   y: number;
+// }
+
+// export enum GridType {
+//   EMPTY = " ",
+//   WALL = "#",
+// }
+
+export interface World {
+  grid: string;
+    world: {
+      agent: any[];
+      box: any[];
+      goal: any[];
+    };
+}
