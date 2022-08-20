4734e102a2518b3e77dc65884c0770416357496f
Oliver Fleckenstein <oliverfl@live.dk>
2022-08-20 23:34:15 +0100

    feat: Listing to agent movements

--- src/world/WorldView.tsx
+++ src/world/WorldView.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { getStaticMap, World } from "../api";
+import { Agent, getEventSource, getStaticMap, World } from "../api";
 import AgentView from "./AgentView";
 import BoxView from "./BoxView";
 import GoalView from "./GoalView";
@@ -7,18 +7,29 @@ import { Grid } from "./Grid";
 
 const WorldView = () => {
   const [world, setWorld] = useState<World | null>(null);
-  useEffect(() => {
-    getStaticMap().then(setWorld);
-  }, []);
+  const [agents, setAgents] = useState<Agent[] | null>(null);
 
-  if (!world) {
+  useEffect(() => {
+    getStaticMap().then((w) => {
+      setWorld(w);
+      setAgents(w.world.agent);
+
+      const source = getEventSource();
+      source.addEventListener("move", (e) => {
+        const agent = JSON.parse(e.data) as Agent;
+        setAgents([agent]);
+      });
+    });
+  }, [agents]);
+
+  if (!world || !agents) {
     return null;
   }
 
   return (
     <>
       <Grid grid={world.grid}>
-        {world.world.agent.map((a) => (
+        {agents!.map((a) => (
           <AgentView key={a.callsign} agent={a} />
         ))}
         {world.world.box.map((b) => (
